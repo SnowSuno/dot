02bd1178d1822903eda0ec6181133924e5ae94f6
snow.kwon <snow.kwon@toss.im>
2024-09-21 01:35:08 +0900

    Add cmp select bindings

---
+++ nvim/lua/plugins/nvim-cmp.lua
@@ -0,0 +1,9 @@
+return {
+  "hrsh7th/nvim-cmp",
+  opts = {
+    mapping = require("cmp").mapping.preset.insert({
+      ["<C-k>"] = require("cmp").mapping.select_prev_item(), -- previous suggestion
+      ["<C-j>"] = require("cmp").mapping.select_next_item(), -- next suggestion
+    }),
+  },
+}
