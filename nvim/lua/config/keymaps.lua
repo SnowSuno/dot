7ba773471eb33a679592c62f9781745ed364b77e
snow.kwon <snow.kwon@toss.im>
2024-09-20 13:48:18 +0900

    init lazyvim

---
+++ nvim/lua/config/keymaps.lua
@@ -0,0 +1,10 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+
+local keymap = vim.keymap
+
+-- window management
+keymap.set("n", "<leader>\\", "<C-w>v", { desc = "Split Window Right" })
+keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make split equal size" })
+keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" })
