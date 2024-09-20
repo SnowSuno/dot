1a3a23f32faad054b92236cb6123492cadd327cb
snow.kwon <snow.kwon@toss.im>
2024-09-21 01:49:48 +0900

    Setup eslint/prettier

---
+++ nvim/lua/plugins/lspconfig.lua
@@ -0,0 +1,14 @@
+return {
+  "neovim/nvim-lspconfig",
+  opts = {
+    servers = {
+      eslint = {
+        settings = {
+          -- helps eslint find the eslintrc when it's placed in a subfolder instead of the cwd root
+          workingDirectories = { mode = "auto" },
+          nodePath = require("lspconfig").util.path.join(LazyVim.root(), ".yarn/sdks"),
+        },
+      },
+    },
+  },
+}
