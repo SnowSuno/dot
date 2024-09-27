e640e60efd2dcba6dbf8a34c5473f379da46a7d2
snow.kwon <snow.kwon@toss.im>
2024-09-27 17:07:26 +0900

    Customize colorscheme

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,7 +1,49 @@
 return {
   {
-    "folke/tokyonight.nvim",
     lazy = true,
-    opts = { style = "moon" },
+    "folke/tokyonight.nvim",
+    opts = {
+      style = "moon",
+      on_colors = function(colors)
+        colors.green = "#8de8ad"
+        colors.purple = "#e3a7fc"
+        -- colors.hint = colors.orange
+        -- colors.error = "#ff0000"
+      end,
+      on_highlights = function(hl, c)
+        local prompt = "#2d3149"
+        hl.TelescopeNormal = {
+          bg = c.bg_dark,
+          fg = c.fg_dark,
+        }
+        hl.TelescopeBorder = {
+          bg = c.bg_dark,
+          fg = c.bg_dark,
+        }
+        hl.TelescopePromptNormal = {
+          bg = prompt,
+        }
+        hl.TelescopePromptBorder = {
+          bg = prompt,
+          fg = prompt,
+        }
+        hl.TelescopePromptTitle = {
+          bg = prompt,
+          fg = prompt,
+        }
+        hl.TelescopePreviewTitle = {
+          bg = c.bg_dark,
+          fg = c.bg_dark,
+        }
+        hl.TelescopeResultsTitle = {
+          bg = c.bg_dark,
+          fg = c.bg_dark,
+        }
+      end,
+    },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = { colorscheme = "tokyonight" },
   },
 }
