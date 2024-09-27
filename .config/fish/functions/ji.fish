e95e6ea1757b5a9c1fe1cb22cc418ff5c74c7fc9
snow.kwon <snow.kwon@toss.im>
2024-09-27 17:07:44 +0900

    add ji ls

--- .config/fish/functions/ji.fish
+++ .config/fish/functions/ji.fish
@@ -1,43 +1,49 @@
+function list_issues
+    jira issue list \
+        -q"project in (INSU, POTI, CAR) AND assignee = currentUser() AND statusCategory != Done" \
+        --plain \
+        --columns key,status,summary \
+        --no-headers
+end
+
 function select_issue
-  jira issue list \
-    -q"assignee = currentUser() AND statusCategory != Done" \
-    --plain \
-    --columns key,status,summary \
-    --no-headers | fzf --height ~100% | awk '{print $1}'
+    list_issues | fzf --height ~100% | awk '{print $1}'
 end
 
 function checkout_jira_issue
-  echo "Checkout 할 이슈를 선택하세요"
-  set -l issue_key (select_issue)
+    echo "Checkout 할 이슈를 선택하세요"
+    set -l issue_key (select_issue)
 
-  
-  if git show-branch $issue_key &>/dev/null; 
-    git checkout $issue_key 
-  else
-    git checkout main
-    git pull
-    git checkout -b $issue_key
-  end
+
+    if git show-branch $issue_key &>/dev/null
+        git checkout $issue_key
+    else
+        git checkout main
+        git pull
+        git checkout -b $issue_key
+    end
 end
 
 function change_jira_issue_status
-  set -l issue_key (select_issue)
-  jira issue move $issue_key
+    set -l issue_key (select_issue)
+    jira issue move $issue_key
 end
 
 function view_current_issue
-  jira issue view $(git b0) --plain
+    jira issue view $(git b0) --plain
 end
 
 function ji
-  switch $argv[1]
-    case c checkout
-      checkout_jira_issue
-    case m move
-      change_jira_issue_status
-    case v view
-      view_current_issue
-    case '*'
-      echo "Unknown command '$argv[1]'"
-  end
+    switch $argv[1]
+        case c checkout
+            checkout_jira_issue
+        case m move
+            change_jira_issue_status
+        case v view
+            view_current_issue
+        case ls
+            list_issues
+        case '*'
+            echo "Unknown command '$argv[1]'"
+    end
 end
