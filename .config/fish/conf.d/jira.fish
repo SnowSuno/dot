bb39d358f31c8c7c412b7c6ab79549c54e0a30cc
Soonho Kwon <percy3368@gmail.com>
2024-11-15 16:17:00 +0900

    update ghpr

--- .config/fish/conf.d/jira.fish
+++ .config/fish/conf.d/jira.fish
@@ -1,7 +1,7 @@
 function get_ticket_summary
     set --local ticket_name $(git b0)
     set --local summary $(curl -s --user "snow.kwon@toss.im:$JIRA_API_TOKEN" "https://tossteam.atlassian.net/rest/api/latest/issue/$ticket_name" | jq ".fields.summary" -r)
-    echo "gh pr create --title \"[$ticket_name] %: $summary\""
+    echo "gh pr create --title \"[$ticket_name] %: $summary\" --draft"
 end
 
 abbr -a ghpr --set-cursor --function get_ticket_summary
