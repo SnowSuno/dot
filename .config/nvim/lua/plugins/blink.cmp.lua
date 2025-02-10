c80dfe1a68f2b673dd3e240b4024cde9c3eefadb
Soonho Kwon <percy3368@gmail.com>
2025-02-10 11:19:43 +0900

    disable ghost text to prevent crashes

--- .config/nvim/lua/plugins/blink.cmp.lua
+++ .config/nvim/lua/plugins/blink.cmp.lua
@@ -2,6 +2,11 @@ return {
   "saghen/blink.cmp",
   version = "v0.7.6",
   opts = {
+    completion = {
+      ghost_text = {
+        enabled = false,
+      },
+    },
     keymap = {
       preset = "enter",
       ["<C-k>"] = { "select_prev" },
