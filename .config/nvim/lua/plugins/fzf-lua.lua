c0424e0f759b48d3603b69f45fc09ddbb9ff45d3
Soonho Kwon <percy3368@gmail.com>
2025-03-12 20:33:18 +0900

    WIP fzf lua config

--- .config/nvim/lua/plugins/fzf-lua.lua
+++ .config/nvim/lua/plugins/fzf-lua.lua
@@ -6,4 +6,25 @@ return {
     { "<leader>fs", LazyVim.pick("live_grep", { root = false }), desc = "Grep (cwd)" },
     { "<leader>fS", LazyVim.pick("live_grep"), desc = "Grep (Root Dir)" },
   },
+  -- opts = function(_, opts)
+  --   local fzf = require("fzf-lua")
+  --   local config = fzf.config
+  --   local actions = fzf.actions
+  --
+  --   return {
+  --     files = {
+  --       cwd_prompt = false,
+  --       actions = {
+  --         ["ctrl-i"] = { actions.toggle_ignore },
+  --         ["ctrl-h"] = { actions.toggle_hidden },
+  --       },
+  --     },
+  --     grep = {
+  --       actions = {
+  --         ["ctrl-i"] = { actions.toggle_ignore },
+  --         ["ctrl-h"] = { actions.toggle_hidden },
+  --       },
+  --     },
+  --   }
+  -- end,
 }
