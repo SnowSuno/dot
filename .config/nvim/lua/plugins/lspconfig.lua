b12b99328a124b7f14a27c3e62974f5db3f6d970
Soonho Kwon <percy3368@gmail.com>
2025-03-30 15:06:34 +0900

    remove c-k keymap

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -1,27 +1,32 @@
-local path = require("lspconfig.util").path
-
-local root = LazyVim.root()
-local is_yarn_pnp = path.is_file(path.join(root, ".pnp.cjs"))
-
 return {
   "neovim/nvim-lspconfig",
-  opts = {
-    servers = {
-      vtsls = {
-        init_options = { hostInfo = "neovim" },
-        settings = {
-          typescript = {
-            tsdk = is_yarn_pnp and path.join(root, ".yarn/sdks/typescript/lib") or nil,
-          },
+  opts = function(_, opts)
+    -- Keymaps
+    local keys = require("lazyvim.plugins.lsp.keymaps").get()
+    keys[#keys + 1] = { "<c-k>", false, mode = "i" }
+
+    -- LSP
+    local path = require("lspconfig.util").path
+
+    local root = LazyVim.root()
+    local is_yarn_pnp = path.is_file(path.join(root, ".pnp.cjs"))
+
+    opts.servers = opts.servers or {}
+
+    opts.servers.vtsls = vim.tbl_deep_extend("force", opts.servers.vtsls or {}, {
+      init_options = { hostInfo = "neovim" },
+      settings = {
+        typescript = {
+          tsdk = is_yarn_pnp and path.join(root, ".yarn/sdks/typescript/lib") or nil,
         },
       },
-      eslint = {
-        settings = {
-          -- helps eslint find the eslintrc when it's placed in a subfolder instead of the cwd root
-          workingDirectories = { mode = "auto" },
-          nodePath = is_yarn_pnp and path.join(root, ".yarn/sdks") or nil,
-        },
+    })
+
+    opts.servers.eslint = vim.tbl_deep_extend("force", opts.servers.eslint or {}, {
+      settings = {
+        workingDirectories = { mode = "auto" },
+        nodePath = is_yarn_pnp and path.join(root, ".yarn/sdks") or nil,
       },
-    },
-  },
+    })
+  end,
 }
